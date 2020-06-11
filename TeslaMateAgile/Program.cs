761864ea798150f63fcfd651daaa6bc547ab9329
Matt Jeanes <mattjeanes23@gmail.com>
2020-06-11 14:00:25 +0100

    Add support for matching TeslaMate database env variables as an alternative to the connection string

--- TeslaMateAgile/Program.cs
+++ TeslaMateAgile/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Data.Common;
 using System.Threading.Tasks;
 using TeslaMateAgile.Data.TeslaMate;
 
@@ -13,12 +16,14 @@ namespace TeslaMateAgile
         public static async Task Main()
         {
             var builder = new HostBuilder()
-                .ConfigureAppConfiguration(configBuilder => {
+                .ConfigureAppConfiguration(configBuilder =>
+                {
                     configBuilder.AddEnvironmentVariables();
                     configBuilder.AddUserSecrets<Program>(true);
                     configBuilder.AddJsonFile("appsettings.json");
                 })
-                .ConfigureLogging((hostContext, loggingBuilder) => {
+                .ConfigureLogging((hostContext, loggingBuilder) =>
+                {
                     var config = hostContext.Configuration;
                     loggingBuilder.AddConfiguration(config.GetSection("Logging"));
                     loggingBuilder.AddConsole();
@@ -26,7 +31,36 @@ namespace TeslaMateAgile
                 .ConfigureServices((hostContext, services) =>
                 {
                     var config = hostContext.Configuration;
-                    services.AddDbContext<TeslaMateDbContext>(o => o.UseNpgsql(config.GetConnectionString("TeslaMate")));
+                    var connectionString = config.GetConnectionString("TeslaMate");
+                    if (string.IsNullOrEmpty(connectionString))
+                    {
+                        Func<string, string> getDatabaseVariable = (string variableName) =>
+                        {
+                            var option = config[variableName];
+                            if (string.IsNullOrEmpty(option))
+                            {
+                                throw new ArgumentNullException(variableName, $"Configuration '{variableName}' or 'ConnectionStrings__TeslaMate' is required");
+                            }
+                            return option;
+                        };
+                        var databaseHost = getDatabaseVariable("DATABASE_HOST");
+                        var databaseName = getDatabaseVariable("DATABASE_NAME");
+                        var databaseUsername = getDatabaseVariable("DATABASE_USER");
+                        var databasePassword = getDatabaseVariable("DATABASE_PASS");
+
+                        var connectionStringBuilder = new NpgsqlConnectionStringBuilder
+                        {
+                            Host = databaseHost,
+                            Database = databaseName,
+                            Username = databaseUsername,
+                            Password = databasePassword
+                        };
+
+                        connectionString = connectionStringBuilder.ConnectionString;
+                    }
+
+                    var builder = new DbConnectionStringBuilder();
+                    services.AddDbContext<TeslaMateDbContext>(o => o.UseNpgsql(connectionString));
                     services.AddHostedService<PriceService>();
                     services.AddOptions<OctopusOptions>()
                         .Bind(config.GetSection("Octopus"))
