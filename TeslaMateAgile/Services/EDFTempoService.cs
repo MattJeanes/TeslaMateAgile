94cace13e981dba9a3aa8be0d07341b539fe5f48
Matt Jeanes <mattjeanes23@gmail.com>
2025-04-14 21:54:11 +0100

    minor patches: - remove unnecessary usings - use correct class for logging injection - use implicit variable assignment where possible - use JsonPropertyName for service provider json answer (and fix property names to match C# convention)

--- TeslaMateAgile/Services/EDFTempoService.cs
+++ TeslaMateAgile/Services/EDFTempoService.cs
@@ -1,15 +1,11 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Globalization;
-using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using TeslaMateAgile.Data;
 using TeslaMateAgile.Data.Options;
 using TeslaMateAgile.Services.Interfaces;
-using System.Collections.Generic;
-using System;
 
 namespace TeslaMateAgile.Services
 {
@@ -21,7 +17,7 @@ namespace TeslaMateAgile.Services
 
         private readonly TimeZoneInfo frenchTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
 
-        public EDFTempoService(HttpClient client, IOptions<EDFTempoOptions> options, ILogger<HomeAssistantService> logger)
+        public EDFTempoService(HttpClient client, IOptions<EDFTempoOptions> options, ILogger<EDFTempoService> logger)
         {
             _client = client;
             _options = options.Value;
@@ -34,9 +30,9 @@ namespace TeslaMateAgile.Services
             from = TimeZoneInfo.ConvertTime(from, frenchTimeZone);
             to = TimeZoneInfo.ConvertTime(to, frenchTimeZone);
 
-            _logger.LogDebug("EDF : Range - {from} -> {to}", from, to);
+            _logger.LogDebug("EDF: Range - {from} -> {to}", from, to);
 
-            string days = "";
+            var days = "";
             // We need also data of previous day, as the ending off peak period end at 6AM the charge start day
             DateTimeOffset currentDate = from.Date.AddDays(-1);
 
@@ -47,17 +43,17 @@ namespace TeslaMateAgile.Services
                 currentDate = currentDate.AddDays(1);
             }
 
-            string url = $"{_options.BaseUrl}?{days}";
+            var url = $"{_options.BaseUrl}?{days}";
 
-            _logger.LogDebug("EDF : URL: {url}", url);
+            _logger.LogDebug("EDF: URL: {url}", url);
 
-            HttpResponseMessage response = await _client.GetAsync(url);
+            var response = await _client.GetAsync(url);
             response.EnsureSuccessStatusCode();
-            string jsonResponse = await response.Content.ReadAsStringAsync();
+            var jsonResponse = await response.Content.ReadAsStringAsync();
 
             _logger.LogDebug("EDF Response: {jsonResponse}", jsonResponse);
 
-            List<TempoDay> data = JsonSerializer.Deserialize<List<TempoDay>>(jsonResponse);
+            var data = JsonSerializer.Deserialize<List<TempoDay>>(jsonResponse);
 
             if (data == null || data.Count == 0)
             {
@@ -66,7 +62,7 @@ namespace TeslaMateAgile.Services
 
             foreach (var item in data)
             {
-                _logger.LogDebug("EDF : TempoDay - Date: {item.dateJour}, Color: {item.codeJour}", item.dateJour, item.codeJour);
+                _logger.LogDebug("EDF: TempoDay - Date: {item.dateJour}, Color: {item.codeJour}", item.DateJour, item.CodeJour);
             }
 
             return GenerateSchedule(data, from, to);
@@ -74,11 +70,11 @@ namespace TeslaMateAgile.Services
 
         private IEnumerable<Price> GenerateSchedule(List<TempoDay> data, DateTimeOffset fromDatetime, DateTimeOffset toDatetime)
         {
-            List<Price> schedList = new List<Price>();
-            List<Tuple<DateTimeOffset, Tuple<TimeSpan, TimeSpan, int, int>, int>> schedule = new List<Tuple<DateTimeOffset, Tuple<TimeSpan, TimeSpan, int, int>, int>>();
+            var schedList = new List<Price>();
+            var schedule = new List<Tuple<DateTimeOffset, Tuple<TimeSpan, TimeSpan, int, int>, int>>();
 
-            // Period Tuple : Start DateTime, End DateTime, color day associated (-1 = previous day), PeakHours (0=Off peak)
-            List<Tuple<TimeSpan, TimeSpan, int, int>> segments = new List<Tuple<TimeSpan, TimeSpan, int, int>>()
+            // Period Tuple: Start DateTime, End DateTime, color day associated (-1 = previous day), PeakHours (0=Off peak)
+            var segments = new List<Tuple<TimeSpan, TimeSpan, int, int>>()
             {
                 new Tuple<TimeSpan, TimeSpan, int, int>(TimeSpan.FromHours(0)+TimeSpan.FromMinutes(0)+TimeSpan.FromSeconds(0), TimeSpan.FromHours(5)+TimeSpan.FromMinutes(59)+TimeSpan.FromSeconds(59)+TimeSpan.FromMilliseconds(999), -1, 0),
                 new Tuple<TimeSpan, TimeSpan, int, int>(TimeSpan.FromHours(6)+TimeSpan.FromMinutes(0)+TimeSpan.FromSeconds(0), TimeSpan.FromHours(21)+TimeSpan.FromMinutes(59)+TimeSpan.FromSeconds(59)+TimeSpan.FromMilliseconds(999), 0, 1),
@@ -86,28 +82,28 @@ namespace TeslaMateAgile.Services
             };
 
             // Price for each period
-            Dictionary<int, decimal> prices = new Dictionary<int, decimal>()
+            var prices = new Dictionary<int, decimal>()
             {
                 {0, _options.BLUE_HC}, {1, _options.BLUE_HP}, {2, _options.WHITE_HC}, {3, _options.WHITE_HP}, {4, _options.RED_HC}, {5, _options.RED_HP}
             };
 
             // For each day, add schedule (Peak/Off peak hours)
-            for (int day = 1; day < data.Count; day++)
+            for (var day = 1; day < data.Count; day++)
             {
-                DateTime dateLocal = DateTime.ParseExact(data[day].dateJour, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                DateTimeOffset date = new DateTimeOffset(dateLocal, frenchTimeZone.GetUtcOffset(dateLocal));
+                var dateLocal = DateTime.ParseExact(data[day].DateJour, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                var date = new DateTimeOffset(dateLocal, frenchTimeZone.GetUtcOffset(dateLocal));
 
                 foreach (var segment in segments)
                 {
-                    int codeJour = data[day + segment.Item3].codeJour;
+                    var codeJour = data[day + segment.Item3].CodeJour;
                     schedule.Add(new Tuple<DateTimeOffset, Tuple<TimeSpan, TimeSpan, int, int>, int>(date, segment, codeJour));
                 }
             }
 
-            // When have we charged ?
+            // When have we charged?
             int startSchedule = 0, stopSchedule = 0;
 
-            for (int i = 0; i < schedule.Count; i++)
+            for (var i = 0; i < schedule.Count; i++)
             {
                 if (fromDatetime.Date == schedule[i].Item1.Date && fromDatetime.TimeOfDay >= schedule[i].Item2.Item1 && fromDatetime.TimeOfDay <= schedule[i].Item2.Item2)
                 {
@@ -120,12 +116,12 @@ namespace TeslaMateAgile.Services
                 }
             }
 
-            _logger.LogDebug("EDF : startSchedule : {startSchedule}, stopSchedule : {stopSchedule}", startSchedule, stopSchedule);
+            _logger.LogDebug("EDF: startSchedule: {startSchedule}, stopSchedule: {stopSchedule}", startSchedule, stopSchedule);
 
             // Get price
-            for (int iter = startSchedule; iter <= stopSchedule; iter++)
+            for (var iter = startSchedule; iter <= stopSchedule; iter++)
             {
-                decimal price = prices[(schedule[iter].Item3 - 1) * 2 + schedule[iter].Item2.Item4];
+                var price = prices[(schedule[iter].Item3 - 1) * 2 + schedule[iter].Item2.Item4];
 
                 if (iter == startSchedule && iter == stopSchedule)
                 {
@@ -150,7 +146,7 @@ namespace TeslaMateAgile.Services
 
             foreach (var item in schedList)
             {
-                _logger.LogDebug("EDF : Price: {item.ValidFrom}, {item.ValidTo}, {item.Value}", item.ValidFrom, item.ValidTo, item.Value);
+                _logger.LogDebug("EDF: Price: {item.ValidFrom}, {item.ValidTo}, {item.Value}", item.ValidFrom, item.ValidTo, item.Value);
             }
 
             return schedList;
@@ -160,8 +156,11 @@ namespace TeslaMateAgile.Services
     // JSON answer format of service provider
     public class TempoDay
     {
-        public string dateJour { get; set; }
-        public int codeJour { get; set; }
-        public string periode { get; set; }
+        [JsonPropertyName("dateJour")]
+        public string DateJour { get; set; }
+        [JsonPropertyName("codeJour")]
+        public int CodeJour { get; set; }
+        [JsonPropertyName("periode")]
+        public string Periode { get; set; }
     }
 }
